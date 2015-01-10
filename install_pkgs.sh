7c2ef79d404ecd7f8369f9f54a450e58e80c5ab4
Farhan Fayyaz <ffayyaz@hsz-t.ch>
2015-01-10 04:35:30 +0100

    update

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ "`whoami`" = "root" ]; then
-    aptitude install `grep -v ^# pkgs_to_install |tr '\n' ' '`
+    aptitude install `grep -v ^# /home/fafa/.fluxbox/pkgs_to_install |tr '\n' ' '`
 fi
