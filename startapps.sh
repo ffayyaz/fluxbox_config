561b660724c5053293b9a2225aa716b4b843e764
Farhan Fayyaz <farhan.fayyaz@netstream.ch>
2016-12-22 16:37:17 +0100

    update

--- startapps.sh
+++ startapps.sh
@@ -58,16 +58,17 @@ if [ "`hostname`" = "fayyaz" ]; then
 
     #[ -x /usr/bin/VBoxManage ] && VBoxManage startvm "Windows 8" &
     # start firstclass
-    wine /home/fafa/.wine/drive_c/Program\ Files/FirstClass/fcc32.exe &
+    #wine /home/fafa/.wine/drive_c/Program\ Files/FirstClass/fcc32.exe &
     [ -x /usr/bin/icedove ] && icedove &
     #chromium --high-dpi-support -force-device-scale-factor=1  --app=http://zabbix.netstream.ch/zabbix/tr_status.php?ddreset=1&sid=c5e867e180599fde &
-    google-chrome --high-dpi-support -force-device-scale-factor=1  --app=http://zabbix.netstream.ch/zabbix/tr_status.php?ddreset=1&sid=c5e867e180599fde &
-    sleep 3
+    #google-chrome --high-dpi-support -force-device-scale-factor=1  --app=http://zabbix.netstream.ch/zabbix/tr_status.php?ddreset=1&sid=c5e867e180599fde &
     #chromium --high-dpi-support --force-device-scale-factor=1  --app=https://intranet.netstream.ch/display/ops/MDW+Environment+Overview#MDWEnvironmentOverview-Live &
-    google-chrome --high-dpi-support --force-device-scale-factor=1  --app=https://intranet.netstream.ch/display/ops/MDW+Environment+Overview#MDWEnvironmentOverview-Live &
-    sleep 3
+    #sleep 5
+    #google-chrome --high-dpi-support --force-device-scale-factor=1  --app=https://intranet.netstream.ch/display/ops/MDW+Environment+Overview#MDWEnvironmentOverview-Live &
     #chromium --high-dpi-support --force-device-scale-factor=1  --app=https://intranet.netstream.ch/display/ops/Manhattan+servers &
-    google-chrome --high-dpi-support --force-device-scale-factor=1  --app=https://intranet.netstream.ch/display/ops/Manhattan+servers &
+    #sleep 5
+    #chromium --high-dpi-support --force-device-scale-factor=1  --app=https://intranet.netstream.ch/display/ops/MDW+Environment+Overview#MDWEnvironmentOverview-Live &
+    #google-chrome --high-dpi-support --force-device-scale-factor=1  --app=https://intranet.netstream.ch/display/ops/Manhattan+servers &
 fi
 
 #____ Netstream notebook
