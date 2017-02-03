09aa9a85fd7c427b1d9ed68ff9c7b5292d299d55
Farhan Fayyaz <farhan.fayyaz@netstream.ch>
2017-02-03 10:21:45 +0100

    update

--- startapps.sh
+++ startapps.sh
@@ -2,12 +2,12 @@
 #
 #_______ Autostart everywhere ______________________________________________
 
-[ -x /usr/bin/xscreensaver ]    && xscreensaver -no-splash&
-[ -x /usr/bin/xclock ]          && xclock -analog -twentyfour -update 1 -padding 1 -render -sharp&
-[ -x /usr/bin/skype ]           && skype &
+[ -f /usr/bin/xscreensaver ]    && xscreensaver -no-splash&
+[ -f /usr/bin/xclock ]          && xclock -analog -twentyfour -update 1 -padding 1 -render -sharp&
+[ -f /usr/bin/skype ]           && skype &
 
 # always start a browser
-if [ -x /usr/bin/chromium ]; then
+if [ -f /usr/bin/chromium ]; then
     chromium --high-dpi-support --force-device-scale=1 &
     sleep 10
 fi
@@ -16,9 +16,9 @@ fi
 
 /sbin/ifconfig|egrep '192.168.85.'
 if [ ${?} -eq 0 ]; then
-    [ -x /usr/bin/pidgin ] && pidgin &
-    [ -x /usr/bin/telegram ] && telegram &
-    [ -x /usr/bin/hipchat ] && hipchat &
+    [ -f /usr/bin/pidgin ] && pidgin &
+    [ -f /usr/bin/telegram ] && telegram &
+    #[ -f /usr/bin/hipchat ] && hipchat &
     #chromium --high-dpi-support --force-device-scale-factor=1  --app=https://netstream.hipchat.com/chat &
     google-chrome --high-dpi-support --force-device-scale-factor=1  --app=https://netstream.hipchat.com/chat &
 fi
@@ -26,8 +26,8 @@ fi
 #_______ Autostart Ganja only ______________________________________________
 
 if [ "`hostname`" = "ganja" ]; then
-    [ -x /usr/bin/xchat ] && xchat &
-    [ -x /usr/bin/icedove ] && icedove &
+    [ -f /usr/bin/xchat ] && xchat &
+    [ -f /usr/bin/icedove ] && icedove &
 
     # recreate playlists
     if [ -f /media/stuff/music/playlists/recreate.sh ]; then
@@ -38,7 +38,7 @@ if [ "`hostname`" = "ganja" ]; then
     chromium --high-dpi-support --force-device-scale=1 --app=https://www.soundcloud.com &
     sleep 2
     chromium --high-dpi-support --force-device-scale=1 --app=https://www.mixcloud.com &
-    [ -x /usr/bin/audacious ] && audacious &
+    [ -f /usr/bin/audacious ] && audacious &
 
     # mouse speed
     xinput --set-prop 9  275 2
@@ -50,9 +50,9 @@ fi
 #_______ Autostart Netstream only __________________________________________
 
 if [ "`hostname`" = "fayyaz" ]; then
-    [ -x /usr/bin/pidgin ] && pidgin &
-    [ -x /usr/bin/hipchat ] && hipchat &
-    [ -x /usr/bin/icedove ] && icedove &
+    [ -f /usr/bin/pidgin ] && pidgin &
+    #[ -f /usr/bin/hipchat ] && hipchat &
+    [ -f /usr/bin/icedove ] && icedove &
     #chromium --high-dpi-support --force-device-scale-factor=1  --app=https://netstream.hipchat.com/chat &
     google-chrome --high-dpi-support --force-device-scale-factor=1  --app=https://netstream.hipchat.com/chat &
 
@@ -61,41 +61,41 @@ if [ "`hostname`" = "fayyaz" ]; then
     # sync chromium to google-chrome
     rsync -arptl --exclude "Singleton*" --delete-before ~/.config/chromium/ ~/.config/google-chrome/
 
-
     # sound
     chromium --high-dpi-support --force-device-scale=1 --app=https://www.soundcloud.com &
     sleep 2
     chromium --high-dpi-support --force-device-scale=1 --app=https://www.mixcloud.com &
-    [ -x /usr/bin/audacious ] && audacious &
+    [ -f /usr/bin/audacious ] && audacious &
 
     # startup windows
-    #[ -x /usr/bin/VBoxManage ] && VBoxManage startvm "Windows 8" &
+    #[ -f /usr/bin/VBoxManage ] && VBoxManage startvm "Windows 8" &
 
     # start firstclass
-    #wine /home/fafa/.wine/drive_c/Program\ Files/FirstClass/fcc32.exe &
+    wine /home/fafa/.wine/drive_c/Program\ Files/FirstClass/fcc32.exe &
 
     # start some desktop windows (zabbix, ...)
     #chromium --high-dpi-support -force-device-scale-factor=1  --app=http://zabbix.netstream.ch/zabbix/tr_status.php?ddreset=1&sid=c5e867e180599fde &
     google-chrome --high-dpi-support -force-device-scale-factor=1  --app=http://zabbix.netstream.ch/zabbix/tr_status.php?ddreset=1&sid=c5e867e180599fde &
-    sleep 15
-    #chromium --high-dpi-support --force-device-scale-factor=1  --app=https://intranet.netstream.ch/display/ops/MDW+Environment+Overview#MDWEnvironmentOverview-Live &
-    google-chrome --high-dpi-support --force-device-scale-factor=1  --app=https://intranet.netstream.ch/display/ops/MDW+Environment+Overview#MDWEnvironmentOverview-Live &
-    sleep 15
-    #chromium --high-dpi-support --force-device-scale-factor=1  --app=https://intranet.netstream.ch/display/ops/Manhattan+servers &
-    google-chrome --high-dpi-support --force-device-scale-factor=1  --app=https://intranet.netstream.ch/display/ops/Manhattan+servers &
+    # sleep 15
+    # #chromium --high-dpi-support --force-device-scale-factor=1  --app=https://intranet.netstream.ch/display/ops/MDW+Environment+Overview#MDWEnvironmentOverview-Live &
+    # google-chrome --high-dpi-support --force-device-scale-factor=1  --app=https://intranet.netstream.ch/display/ops/MDW+Environment+Overview#MDWEnvironmentOverview-Live &
+    # sleep 15
+    # #chromium --high-dpi-support --force-device-scale-factor=1  --app=https://intranet.netstream.ch/display/ops/Manhattan+servers &
+    # google-chrome --high-dpi-support --force-device-scale-factor=1  --app=https://intranet.netstream.ch/display/ops/Manhattan+servers &
 fi
 
 #____ Netstream notebook
+
 if [ "`hostname`" = "nacho" ]; then
-    [ -x /usr/bin/syndaemon ] && syndaemon -k -i 0.5&
+    [ -f /usr/bin/syndaemon ] && syndaemon -k -i 0.5&
 fi
 
 #_______ Autostart mobile only _____________________________________________
 
 if [ "`hostname`" = "mobile" ]; then
     #cpufreqterm &
-    [ -x /usr/sbin/thinkfan ] && sudo thinkfan -s1 -b0 -z -n 2>&1 &> /tmp/thinkfan.log&
-    [ -x /usr/bin/syndaemon ] && syndaemon -k -i 0.5&
+    [ -f /usr/sbin/thinkfan ] && sudo thinkfan -s1 -b0 -z -n 2>&1 &> /tmp/thinkfan.log&
+    [ -f /usr/bin/syndaemon ] && syndaemon -k -i 0.5&
 fi
 
 #_______ Autostart everywhere (final)_______________________________________
