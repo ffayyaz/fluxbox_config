8d29d52e9c117ca300a08b08910c7675cec4f9f4
Farhan Fayyaz <farhan.fayyaz@netstream.ch>
2017-09-25 13:04:40 +0200

    update

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [ "`whoami`" = "root" ]; then
     grep -v '^#' /home/fafa/.fluxbox/pkgs_to_install|while read PKG; do
-        dpkg -l|awk '/^ii/ {print $2}'|grep -q "^${PKG}$" && echo "${PKG} already installed" || apt-get install -y ${PKG} 2 > ${0}.errors
+        echo -ne "$PKG: "
+        dpkg -l|awk '/^ii/ {print $2}'|grep -q "^${PKG}$" && echo "${PKG} already installed" || apt-get install -y ${PKG}
+        echo ""
     done
 fi
