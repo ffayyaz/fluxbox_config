65c58ab49fd35791b3eea2d568294facf1108a20
Farhan Fayyaz <ffayyaz@hsz-t.ch>
2020-11-13 18:27:21 +0100

    update

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -2,7 +2,11 @@
 if [ "`whoami`" = "root" ]; then
     grep -v '^#' /home/fafa/.fluxbox/pkgs_to_install|while read PKG; do
         echo -ne "$PKG: "
-        dpkg -l|awk '/^ii/ {print $2}'|grep -q "^${PKG}$" && echo "${PKG} already installed" || apt-get install -y ${PKG}
+        if dpkg -l|awk '/^ii/ {print $2}'|grep -q "^${PKG}$"; then
+            echo "${PKG} already installed"
+        else
+             aptitude install ${PKG}
+        fi
         echo ""
     done
 fi
