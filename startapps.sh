ef4027a2bf745100a24a1e57c5f9381ca3540534
Farhan Fayyaz <ffayyaz@hsz-t.ch>
2021-08-25 17:29:58 +0200

    update

--- startapps.sh
+++ startapps.sh
@@ -3,28 +3,22 @@
 #_______ Autostart everywhere ______________________________________________
 
 # start desktop apps
-{
-    xscreensaver -no-splash &
-    xclock -analog -twentyfour -update 1 -padding 1 -render -sharp &
-}
+xscreensaver -no-splash &
+xclock -analog -twentyfour -update 1 -padding 1 -render -sharp &
 
 # start 2 terminals
-{
-    urxvt -hold -e bash -c "screen -dR root" &
-    urxvt -hold -e bash -c "screen -dR fafa" &
-}
+urxvt -hold -e bash -c "screen -dR root" &
+urxvt -hold -e bash -c "screen -dR fafa" &
 
 # start chats
-{
-    skypeforlinux &
-    (telegram-desktop 2>&1>/dev/null &)
-    (slack 2>&1>/dev/null &)
-    (signal-desktop 2>&1>/dev/null &)
-    chromium --app=https://web.whatsapp.com/ &
-}
+#skypeforlinux &
+#(telegram-desktop 2>&1>/dev/null &)
+#(slack 2>&1>/dev/null &)
+#(signal-desktop 2>&1>/dev/null &)
+#chromium --app=https://web.whatsapp.com/ &
 
 # always start a browser
-chromium &
+#chromium &
 
 #_______ Autostart Home only _______________________________________________
 
@@ -32,34 +26,34 @@ chromium &
 
 #_______ Autostart Ganja only ______________________________________________
 
-if [ "$(hostname)" = "ganja" ]; then
-    # remote terminals
-    #urxvt -hold -name URxvt-remote -tn remoteterm & #-e bash -c "ssh mobile" &
-    #urxvt -hold -name URxvt-remote -tn remoteterm & #-e bash -c "ssh mobile-old" &
-
-    thunderbird &
-
-    # sound
-    chromium --app="https://www.soundcloud.com" &
-    chromium --app="https://www.mixcloud.com" &
-    audacious &
-    pavucontrol &
-fi
+#if [ "$(hostname)" = "ganja" ]; then
+#    # remote terminals
+#    #urxvt -hold -name URxvt-remote -tn remoteterm & #-e bash -c "ssh mobile" &
+#    #urxvt -hold -name URxvt-remote -tn remoteterm & #-e bash -c "ssh mobile-old" &
+#
+#    thunderbird &
+#
+#    # sound
+#    chromium --app="https://www.soundcloud.com" &
+#    chromium --app="https://www.mixcloud.com" &
+#    audacious &
+#    pavucontrol &
+#fi
 
 #_______ Autostart everywhere (final)_______________________________________
 
-# they need to be started at the end, otherwise
-# they are not drawn correctly (they overlap the slit)
-# BULLCRAP workaround
-#messagesterm &
-#syslogterm &
-#debuglogterm &
-#authlogterm &
-#kernlogterm &
-#topterm &
-mixerterm &
-clockterm &
-#calterm &
+## they need to be started at the end, otherwise
+## they are not drawn correctly (they overlap the slit)
+## BULLCRAP workaround
+##messagesterm &
+##syslogterm &
+##debuglogterm &
+##authlogterm &
+##kernlogterm &
+##topterm &
+#mixerterm &
+#clockterm &
+##calterm &
 
 #_______ random crap________________________________________________________
 
