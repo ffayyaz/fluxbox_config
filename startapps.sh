9d5f9758ba45e7a3f1ac0797ccf27a7e4dac014c
Farhan Fayyaz <ffayyaz@hsz-t.ch>
2023-04-03 21:16:46 +0200

    update

--- startapps.sh
+++ startapps.sh
@@ -22,8 +22,8 @@ sleep 1
 urxvt -title USER -hold -e bash -c "screen -dR fafa" &
 
 # start chats
-#(telegram-desktop 2>&1>/dev/null &)
-chromium --app=https://web.telegram.org/z/ &
+(telegram-desktop 2>&1>/dev/null &)
+#chromium --app=https://web.telegram.org/z/ &
 sleep 1
 skypeforlinux &
 sleep 1
